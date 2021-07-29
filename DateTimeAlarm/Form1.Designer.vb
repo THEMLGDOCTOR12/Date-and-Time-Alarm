ba86b13513f4f52eb60b39a1f4e4cdadde217a75
THEMLGDOCTOR12 <75111234+THEMLGDOCTOR12@users.noreply.github.com>
2021-07-29 10:14:18 +1000

    Add files via upload

---
+++ DateTimeAlarm/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.dgvOutput = New System.Windows.Forms.DataGridView()
+        Me.dtpDate = New System.Windows.Forms.DateTimePicker()
+        Me.txtTime = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtEvent = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.dgvOutput, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvOutput
+        '
+        Me.dgvOutput.AllowUserToAddRows = False
+        Me.dgvOutput.AllowUserToDeleteRows = False
+        Me.dgvOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvOutput.Location = New System.Drawing.Point(12, 12)
+        Me.dgvOutput.Name = "dgvOutput"
+        Me.dgvOutput.ReadOnly = True
+        Me.dgvOutput.RowTemplate.Height = 25
+        Me.dgvOutput.Size = New System.Drawing.Size(297, 337)
+        Me.dgvOutput.TabIndex = 0
+        '
+        'dtpDate
+        '
+        Me.dtpDate.Location = New System.Drawing.Point(315, 36)
+        Me.dtpDate.Name = "dtpDate"
+        Me.dtpDate.Size = New System.Drawing.Size(216, 23)
+        Me.dtpDate.TabIndex = 1
+        Me.dtpDate.Value = New Date(2021, 6, 23, 0, 0, 0, 0)
+        '
+        'txtTime
+        '
+        Me.txtTime.Location = New System.Drawing.Point(537, 36)
+        Me.txtTime.Name = "txtTime"
+        Me.txtTime.Size = New System.Drawing.Size(100, 23)
+        Me.txtTime.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(404, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 21)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Date"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(563, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 21)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Time"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(438, 78)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(93, 21)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Alarm Label"
+        '
+        'txtEvent
+        '
+        Me.txtEvent.Location = New System.Drawing.Point(315, 115)
+        Me.txtEvent.Name = "txtEvent"
+        Me.txtEvent.Size = New System.Drawing.Size(322, 23)
+        Me.txtEvent.TabIndex = 6
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(392, 157)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(184, 34)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(646, 358)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtEvent)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtTime)
+        Me.Controls.Add(Me.dtpDate)
+        Me.Controls.Add(Me.dgvOutput)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Date and Time Alarm"
+        CType(Me.dgvOutput, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents dgvOutput As DataGridView
+    Friend WithEvents dtpDate As DateTimePicker
+    Friend WithEvents txtTime As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtEvent As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ DateTimeAlarm/Form1.vb
@@ -0,0 +1,126 @@
+﻿'Clock Icon provided by Icons8 - https://icons8.com/icons/set/clock'
+Imports System.Xml
+Imports System.Data
+Public Class Form1
+    Dim N As String = 0
+    Dim DateTest As Boolean = False
+    Dim TimeTest As Boolean = False
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim filelocation As String = My.Application.Info.DirectoryPath & "\Alarms.xml"
+        Dim CurrentDate As String = DateAndTime.Today
+        Dim CurrentTime As String = TimeOfDay.ToString("h:mm tt")
+
+        Dim ds As New DataSet
+
+        Dim doc As New System.Xml.XmlDocument()
+        doc.Load(filelocation)
+
+        For Each data As System.Xml.XmlElement In doc.DocumentElement.ChildNodes
+
+            For Each field As System.Xml.XmlElement In data.ChildNodes
+                'MessageBox.Show(field.Name & " : " & field.InnerText)
+                If field.InnerText = CurrentDate Then
+                    DateTest = True
+                End If
+                If field.InnerText <= CurrentTime Then
+                    TimeTest = True
+                End If
+            Next
+            If DateTest = True And TimeTest = True Then
+                MsgBox("Alarm")
+            End If
+        Next
+        DateTest = False
+        TimeTest = False
+        ds.ReadXml(filelocation)
+        dgvOutput.DataSource = ds.Tables(0)
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim CurrentDate As String = DateAndTime.Today
+        Dim CurrentTime As String = TimeOfDay.ToString("h:mm tt")
+        Dim SetDate As String = dtpDate.Value
+        Dim SetTime As String = txtTime.Text
+        Dim SetName As String = txtEvent.Text
+        Dim filelocation As String = My.Application.Info.DirectoryPath & "\Alarms.xml"
+        Dim tempfile As String = My.Application.Info.DirectoryPath & "\Temp.xml"
+        Dim ds As New DataSet
+
+        Dim doc As New System.Xml.XmlDocument()
+        doc.Load(filelocation)
+
+        For Each data As System.Xml.XmlElement In doc.DocumentElement.ChildNodes
+
+            For Each field As System.Xml.XmlElement In data.ChildNodes
+                'MessageBox.Show(field.Name & " : " & field.InnerText)
+                If field.InnerText = CurrentDate Then
+                    DateTest = True
+                End If
+                If field.InnerText <= CurrentTime Then
+                    TimeTest = True
+                End If
+            Next
+            If DateTest = True And TimeTest = True Then
+                MsgBox("Alarm")
+            End If
+        Next
+        DateTest = False
+        TimeTest = False
+
+        'SetDate <= CurrentDate Then
+        'MsgBox("Set Date Reached")
+        'Else
+        'MsgBox("Set Date Not Reached")
+        'End If
+        If System.IO.File.Exists(filelocation) Then
+            Dim writer As New XmlTextWriter(tempfile, System.Text.Encoding.UTF8)
+            writer.WriteStartDocument(True)
+
+            writer.Formatting = Formatting.Indented
+            writer.Indentation = 2
+
+            writer.WriteStartElement("Alarm")
+            CreateXmlRec(SetName, SetDate, SetTime, writer)
+            writer.WriteEndElement()
+            writer.WriteEndDocument()
+            writer.Close()
+            Dim x1 As New DataSet
+            x1.ReadXml(filelocation)
+            Dim x2 As New DataSet
+            x2.ReadXml(tempfile)
+
+            x1.Merge(x2)
+            x1.WriteXml(filelocation)
+        Else
+            Dim writer As New XmlTextWriter(filelocation, System.Text.Encoding.UTF8)
+            writer.WriteStartDocument(True)
+
+            writer.Formatting = Formatting.Indented
+            writer.Indentation = 2
+
+            writer.WriteStartElement("Alarm")
+            CreateXmlRec(SetName, SetDate, SetTime, writer)
+            writer.WriteEndElement()
+            writer.WriteEndDocument()
+            writer.Close()
+        End If
+        ds.ReadXml(filelocation)
+        dgvOutput.DataSource = ds.Tables(0)
+    End Sub
+    Private Function CreateXmlRec(ByVal IncomingName As String, ByVal IncomingDate As String, ByVal IncomingTime As String, ByVal writer As XmlTextWriter)
+        writer.WriteStartElement("Alarm")
+
+        writer.WriteStartElement("Name")
+        writer.WriteString(IncomingName)
+        writer.WriteEndElement()
+
+        writer.WriteStartElement("Date")
+        writer.WriteString(IncomingDate)
+        writer.WriteEndElement()
+
+        writer.WriteStartElement("Time")
+        writer.WriteString(IncomingTime)
+        writer.WriteEndElement()
+
+        writer.WriteEndElement()
+    End Function
+End Class
